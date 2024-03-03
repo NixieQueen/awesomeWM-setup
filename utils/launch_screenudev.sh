19e42dea212b4dcb86ef0e0e898ad7357dbea860
NixieQueen <thestampyliker@gmail.com>
2024-03-03 14:33:43 +0100

    built the udev binary to avoid conflicts

--- utils/launch_screenudev.sh
+++ utils/launch_screenudev.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 _launchudev() {
 	if ! pgrep -f "screenudev.py"; then
-		source $SCRIPT_DIR/venv/bin/activate
 		$SCRIPT_DIR/screenudev.py &
 	fi
 }
